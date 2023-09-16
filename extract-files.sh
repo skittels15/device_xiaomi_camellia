3aacec592f6accec3edb3f265c0644e3593029e9
Mashopy <eliasgheeraert@gmail.com>
2023-09-16 19:01:21 +0200

    camellia: Fixup media C2 registered version
    Our C2 service target media C2 HIDL 1.2 version, not 1.1

--- extract-files.sh
+++ extract-files.sh
@@ -83,6 +83,9 @@ function blob_fixup {
 	vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so)
             "${PATCHELF}" --replace-needed "libsensorndkbridge.so" "libsensorndkbridge-hidl.so" "$2"
             ;;
+	vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml)
+            sed -i 's/1.1/1.2/' "$2"
+            ;;
     esac
 }
 
